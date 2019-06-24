aacac428b33b019da7ce097b6f83e07697f5903e
毕宏飞 <bihongfei@zhigui.com>
2019-06-24 16:53:56 +0800

    create WechatGroup project

---
+++ README.md
@@ -0,0 +1,44 @@
+## WechatGroup
+WechatGroup可以实现监控某个群组信息，实时发送被监控群消息到指定群组中，支持文字、分享、图片、语音等转发功能
+
+### 1、安装环境
+
+```
+pip install itchat
+```
+
+### 2、文件说明
+
+**wechat.ini** 配置文件
+
+```
+[rooms]
+src_room=src		# 被监控群
+dest_room=dest		# 发送到指定群
+```
+
+**WechaGroup.py** 执行脚本，实时监控群消息并转发
+
+```
+python WechatGroup.py
+```
+
+### 3、使用帮助
+
+#### 3.1 加入通讯录
+把要监听的群以及接收的群加入到微信通讯录，如下图所示：(**注意：两个群都加**)  
+
+![](images/WechatIMG1.jpeg)  
+![](images/WechatIMG2.jpeg)
+#### 3.2 执行脚本
+
+```
+python WechatGroup.py
+```
+**扫描二维码确认登录**
+
+#### 3.3 实现效果
+**被监听群（src）**
+![](images/WechatIMG3.jpeg)  
+**接收群（dest）**
+![](images/WechatIMG4.jpeg)  
---
+++ WechatGroup.py
@@ -0,0 +1,52 @@
+# coding:utf8
+import os
+import sys
+import ConfigParser
+reload(sys)
+sys.setdefaultencoding( "utf8" ) 
+import itchat
+from itchat.content import *
+
+
+cf = ConfigParser.ConfigParser()
+cf.read('wechat.ini')
+src_room=cf.get('rooms','src_room')
+dest_room=cf.get('rooms','dest_room')
+
+def get_group(room):
+    rooms = itchat.get_chatrooms(update=True,contactOnly=True)
+    for i in rooms:
+        if i["NickName"] == room:
+            return i["UserName"]    
+
+
+@itchat.msg_register([TEXT, SHARING], isGroupChat=True)
+def forward_text(msg):
+    now_roomid = msg['FromUserName']    
+    if now_roomid == get_group(src_room):
+        user_name = msg['ActualNickName']
+        if msg['Type'] == TEXT:
+            content = msg['Content']
+            itchat.send('%s（%s）:\n\n%s' % (user_name,src_room,content),get_group(dest_room) )
+        elif msg['Type'] == SHARING:
+            content = msg['Text']
+            url = msg["Url"]
+            itchat.send('%s（%s）:\n%s\n%s' % (user_name,src_room,content, url), get_group(dest_room))
+         
+@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO], isGroupChat=True)
+def forward_media(msg):
+    now_roomid = msg['FromUserName']
+    if now_roomid == get_group(src_room):
+        user_name = msg['ActualNickName']
+        media_name = msg['FileName']
+        if media_name[-4:] == '.gif':
+            return
+        else:
+            msg['Text'](media_name)
+            typeSymbol = {PICTURE: 'img',VIDEO: 'vid', }.get(msg['Type'], 'fil')
+            itchat.send('@%s@%s' % (typeSymbol, media_name),get_group(dest_room))
+            # itchat.send('@%s@%s' % ('img' if msg['Type'] == 'Picture' else 'fil', msg['FileName']),get_group(dest_room))
+            os.remove(media_name)
+
+itchat.auto_login(hotReload=True)
+itchat.run()
